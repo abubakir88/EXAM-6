31f886d5c422c254bfd0e8e4855ec7cfda43e859
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-03-23 02:06:28 +0500

    add loader

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,327 +1,118 @@
-import {
-  Button,
-  Checkbox,
-  Flex,
-  Form,
-  Image,
-  Input,
-  Modal,
-  Select,
-  Space,
-  Table,
-} from "antd";
-import Search from "antd/es/input/Search";
-import { Fragment, useCallback, useEffect, useState } from "react";
-import request from "../../server";
+import React, { useEffect, useState } from "react";
+import "../../scss/Add.scss";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Sidebar from "./Sidebar/Sidebar";
+const Add = ({ user }) => {
+  const [post, setPost] = useState([
+    {
+      id: "",
+      brand: "",
+      price: "",
+      discountPercentage: "",
+    },
+  ]);
 
-const StudentsPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalLoading, setModalLoading] = useState(false);
-  const [teachers, setTeachers] = useState();
-  const [studentData, setStudentData] = useState([]);
-  const [dataForId, setDataForId] = useState("");
-  const [selected, setSelected] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState("");
-  const [teacherId, setTeacherId] = useState();
-
-  const [id, setId] = useState(null);
-  const [form] = Form.useForm();
-
-  const handleChange = (value) => {
-    setSelected(value);
-    setTeacherId(value);
-  };
-
-  const showModal = () => {
-    form.resetFields();
-    setSelected(null);
-    setIsModalOpen(true);
-  };
-
-  const getStudents = useCallback(async () => {
+  const addNew = async () => {
     try {
-      setLoading(true);
-      let { data } = await request.get(`teacher/${teacherId}/student`);
-      setStudentData(data);
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setLoading(false);
-    }
-  }, [teacherId]);
-
-  const handleOk = async () => {
-    try {
-      setModalLoading(true);
-      let values = await form.validateFields();
-      if (selected === null) {
-        await request.post(`teacher/${teacherId}/student`, values);
-      } else {
-        await request.put(`teacher/${teacherId}/student/${selected}`, values);
-      }
-      getStudents();
-      setIsModalOpen(false);
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setModalLoading(false);
+      const response = await axios.post("http://localhost:3000/products", post);
+      fetchProducts();
+    } catch (error) {
+      console.log(error);
     }
   };
-
-  useEffect(() => {
-    const getTeachers = async () => {
-      try {
-        let { data } = await request.get(`teacher`);
-        setTeachers(data);
-        let { data: anotherData } = await request.get(
-          `teacher?filter=${selected}`
-        );
-        setDataForId(anotherData);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    getStudents();
-
-    getTeachers();
-  }, [selected, id, getStudents]);
-
-  const editStudent = async (id) => {
-    try {
-      form.resetFields();
-      setIsModalOpen(true);
-      setSelected(id);
-      let { data } = await request.get(`teacher/${teacherId}/student/${id}`);
-      form.setFieldsValue(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const deleteStudent = (data) => {
-    Modal.confirm({
-      title: "Do you want to delete this student ?",
-      onOk: async () => {
-        await request.delete(`teacher/${teacherId}/student/${data}`);
-        getStudents();
-      },
-    });
-  };
-
-  let names = [];
-  let ids = [];
-  for (let i in teachers) {
-    names.push(teachers[i].firstName);
-    ids.push(teachers[i].id);
-  }
-  console.log(teachers);
-  let options = names.map((name) => {
-    return {
-      label: name,
-    };
-  });
-  options = ids.map((name) => {
-    return {
-      value: name,
-    };
-  });
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const columns = [
-    {
-      title: "Image",
-      dataIndex: "image",
-      key: "image",
-      render: (data) => {
-        return <Image height={50} src={data} />;
-      },
-    },
-    {
-      title: "FirstName",
-      dataIndex: "firstName",
-      key: "firstName",
-    },
-    {
-      title: "LastName",
-      dataIndex: "lastName",
-      key: "lastName",
-    },
-    {
-      title: "Phone Number",
-      dataIndex: "phoneNumber",
-      key: "phoneNumber",
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
-    },
-    {
-      title: "isWork",
-      dataIndex: "isWork",
-      key: "isWork",
-      render: (data) => (data ? "Yes" : "No"),
-    },
-    {
-      title: "Action",
-      dataIndex: "id",
-      key: "id",
-      render: (data) => (
-        <Space size="middle">
-          <Button onClick={() => editStudent(data)} type="primary">
-            Edit
-          </Button>
-          <Button onClick={() => deleteStudent(data)} danger type="primary">
-            Delete
-          </Button>
-        </Space>
-      ),
-    },
-  ];
 
   return (
-    <Fragment>
-      <Flex align="center" justify="space-between" gap={30}>
-        <h1>Students</h1>
-        <Search
-          placeholder="Searching students .."
-          onChange={(e) => setSearch(e.target.value)}
-          size="large"
-        />
-        <Flex className="filter-box" align="center" justify="right" gap={20}>
-          <Select
-            defaultValue="Teachers"
-            onChange={handleChange}
-            options={options}
-          />
-          <Button
-            onClick={showModal}
-            className="modal-btn"
-            type="dashed"
-            size="large"
-          >
-            Add student
-          </Button>
-        </Flex>
-      </Flex>
+    <div>
+      <main className="addMain">
+        <Sidebar />
+        <section className="sec1">
+          <h1>Новый товар</h1>
+          <p>Главная / Товары / Новый товар</p>
+        </section>
+        <section className="sec2">
+          <div className="all-inputs">
+            <div className="inline-inputs">
+              <label
+                id="named"
+                className="inp-name mb-2 text-center justify-content-center mt-5"
+              >
+                <p>Название *</p>
+              </label>
+              <input
+                type="number"
+                id="id"
+                className="form-control w-75 mb-3"
+                required
+                onChange={(e) => setPost({ ...post, id: e.target.value })}
+                value={post.id}
+                name="id"
+              />
 
-      <Table
-        scroll={{
-          x: 1000,
-        }}
-        dataSource={studentData}
-        title={() => <Fragment></Fragment>}
-        loading={loading}
-        columns={columns}
-        pagination={false}
-      />
-      <Modal
-        title="Student Info"
-        open={isModalOpen}
-        confirmLoading={modalLoading}
-        onOk={handleOk}
-        okText={selected === null ? "Add student" : "Save student"}
-        onCancel={closeModal}
-        initialValues={{
-          isMarried: false,
-        }}
-      >
-        <Form
-          form={form}
-          className="modal-form"
-          name="form"
-          labelCol={{
-            span: 24,
-          }}
-          wrapperCol={{
-            span: 24,
-          }}
-          autoComplete="off"
-        >
-          <Form.Item
-            className="form-item"
-            label="First Name"
-            name="firstName"
-            rules={[
-              {
-                required: true,
-                message: "Please fill!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+              <div className="brand">
+                <label className="mb-3">Бренд *</label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="form-control"
+                  required
+                  onChange={(e) => setPost({ ...post, brand: e.target.value })}
+                  value={post.brand}
+                />
+              </div>
+              <div
+                id="cent"
+                className="inp-price-others d-flex justify-content-between mb-4 "
+              >
+                <div>
+                  <label className="mb-2">Цена</label>
+                  <input
+                    type="number"
+                    name="price"
+                    id="price"
+                    className="form-control"
+                    required
+                    onChange={(e) =>
+                      setPost({ ...post, price: e.target.value })
+                    }
+                    value={post.price}
+                  />
+                </div>
+              </div>
 
-          <Form.Item
-            className="form-item"
-            label="Last Name"
-            name="lastName"
-            rules={[
-              {
-                required: true,
-                message: "Please fill!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Phone Number"
-            name="phoneNumber"
-            rules={[
-              {
-                required: true,
-                message: "Please fill!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Please fill!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Image URL"
-            name="image"
-            rules={[
-              {
-                required: true,
-                message: "Please fill!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            className="form-item"
-            name="isWork"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 0,
-              span: 16,
-            }}
-          >
-            <Checkbox>isWork</Checkbox>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </Fragment>
+              <div className="inp-price-others d-flex justify-content-between mb-5">
+                <div>
+                  <label className="mb-2 mt-4">Цена со скидкой</label>
+                  <input
+                    type="number"
+                    name="discountPercentage"
+                    id="discountPercentage"
+                    className="form-control"
+                    required
+                    onChange={(e) =>
+                      setPost({ ...post, discountPercentage: e.target.value })
+                    }
+                    value={post.discountPercentage}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="buts">
+            <Link to="/">
+              <button type="submit" className="but1" onClick={addNew}>
+                Save
+              </button>
+            </Link>
+            <Link to="/">
+              <button className="but1">Cancel</button>
+            </Link>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
 
-export default StudentsPage;
+export default Add;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,6 +11,9 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 /////
 
+// LOADER //////
+import ScaleLoader from "react-spinners/ScaleLoader";
+// LOADER //////
 const Home = () => {
   /// MODAL //////
   const [show, setShow] = useState(false);
@@ -42,6 +45,16 @@ const Home = () => {
 
   // SEARCH finish //////////////////////////////////
 
+  // LOADER START //////////////////////////////////
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  // LOSDER FINISH //////////////////////////////////
+
   useEffect(() => {
     const fetchPanel = async () => {
       try {
@@ -58,152 +71,166 @@ const Home = () => {
     <>
       <Sidebar />
       <Header />
-      <div className="for_bg">
-        <div className="content mt-4 w-75">
-          <div className="d-flex justify-content-between bg-white p-4  ">
-            <h3>Все товары ({panel.length})</h3>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Control
-                  type="text"
-                  placeholder="Qidiruv..."
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-              </Form.Group>
-            </Form>
-          </div>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th className="text-body-tertiary text-start" scope="col">
-                  Наименование
-                </th>
-                <th className="text-body-tertiary" scope="col">
-                  Артикул
-                </th>
-                <th className="text-body-tertiary" scope="col">
-                  Бренд
-                </th>
-                <th className="text-body-tertiary" scope="col">
-                  Цена
-                </th>
-                <th className="text-body-tertiary" scope="col">
-                  Цена со скидкой
-                </th>
-                <th className="text-body-tertiary" scope="col">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            {/* <tbody> */}
-            {panel.length > 0 && (
-              <tbody>
-                {panel
-                  .filter((panel) =>
-                    panel.brand.toLowerCase().includes(searchTerm.toLowerCase())
-                  )
-                  .map((panel) => (
-                    <>
-                      <tr>
-                        <th className="text-start"> Товар {panel.id}</th>
-                        <th className="fw-normal">{panel.rating}</th>
-                        <th className="fw-normal">{panel.brand}</th>
-                        <th className="fw-normal">{panel.price}</th>
-                        <th className="fw-normal">{panel.stock}</th>
-                        <th className="d-flex gap-1">
-                          <button className="btn btn-primary">Edit</button>
-                          <Button
-                            variant="danger "
-                            onClick={() =>
-                              confirm("Are you sure you want to?")
-                                ? deleteItem(panel.id)
-                                : false
-                            }
-                          >
-                            Delete
-                          </Button>
-                        </th>
-                      </tr>
-                    </>
-                  ))}
-              </tbody>
-            )}
-            {/* </tbody> */}
-          </table>
-        </div>
-        <Button
-          variant="primary"
-          onClick={handleShow}
-          className="btn btn-success text-start"
-        >
-          + Новый товар
-        </Button>
-        <Modal backdrop="static" show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Product</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>Артикул</Form.Label>
-                <Form.Control
-                  type="number"
-                  placeholder="Артикул"
-                  autoFocus
-                  required
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>Бренд</Form.Label>
-                <Form.Control
-                  type="brand"
-                  placeholder="Artel milliy brand"
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Цена</Form.Label>
-                <Form.Control
-                  as="input"
-                  type="number"
-                  rows={3}
-                  placeholder="Цена"
-                />
-              </Form.Group>
 
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Цена со скидкой</Form.Label>
-                <Form.Control
-                  as="input"
-                  type="number"
-                  rows={3}
-                  placeholder="Цена со скидкой"
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button type="submit" variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+      {loading ? (
+        <ScaleLoader
+          className="text-center mt-5 pt-5 "
+          color={"plum"}
+          loading={loading}
+          size={50}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <div className="for_bg">
+          <div className="content mt-4 w-75">
+            <div className="d-flex justify-content-between bg-white p-4  ">
+              <h3>Все товары ({panel.length})</h3>
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder="Qidiruv..."
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </Form.Group>
+              </Form>
+            </div>
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th className="text-body-tertiary text-start" scope="col">
+                    Наименование
+                  </th>
+                  <th className="text-body-tertiary" scope="col">
+                    Артикул
+                  </th>
+                  <th className="text-body-tertiary" scope="col">
+                    Бренд
+                  </th>
+                  <th className="text-body-tertiary" scope="col">
+                    Цена
+                  </th>
+                  <th className="text-body-tertiary" scope="col">
+                    Цена со скидкой
+                  </th>
+                  <th className="text-body-tertiary" scope="col">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              {/* <tbody> */}
+              {panel.length > 0 && (
+                <tbody>
+                  {panel
+                    .filter((panel) =>
+                      panel.brand
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    )
+                    .map((panel) => (
+                      <>
+                        <tr>
+                          <th className="text-start"> Товар {panel.id}</th>
+                          <th className="fw-normal">{panel.rating}</th>
+                          <th className="fw-normal">{panel.brand}</th>
+                          <th className="fw-normal">{panel.price}</th>
+                          <th className="fw-normal">{panel.stock}</th>
+                          <th className="d-flex gap-1">
+                            <button className="btn btn-primary">Edit</button>
+                            <Button
+                              variant="danger "
+                              onClick={() =>
+                                confirm("Are you sure you want to?")
+                                  ? deleteItem(panel.id)
+                                  : false
+                              }
+                            >
+                              Delete
+                            </Button>
+                          </th>
+                        </tr>
+                      </>
+                    ))}
+                </tbody>
+              )}
+              {/* </tbody> */}
+            </table>
+          </div>
+          <Button
+            variant="primary"
+            onClick={handleShow}
+            className="btn btn-success text-start"
+          >
+            + Новый товар
+          </Button>
+          <Modal backdrop="static" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add Product</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Артикул</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Артикул"
+                    autoFocus
+                    required
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Бренд</Form.Label>
+                  <Form.Control
+                    type="brand"
+                    placeholder="Artel milliy brand"
+                    autoFocus
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>Цена</Form.Label>
+                  <Form.Control
+                    as="input"
+                    type="number"
+                    rows={3}
+                    placeholder="Цена"
+                  />
+                </Form.Group>
+
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>Цена со скидкой</Form.Label>
+                  <Form.Control
+                    as="input"
+                    type="number"
+                    rows={3}
+                    placeholder="Цена со скидкой"
+                  />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button type="submit" variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      )}
     </>
   );
 };
